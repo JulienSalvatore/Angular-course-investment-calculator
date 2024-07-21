ad15a4328ebd6b5d85d8c83e31d63de1d0cd4518
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-21 20:46:40 +0200

    add pipe to currencies values

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
