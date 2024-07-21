d24ffa3cf46451668a7b448a439ba1166e393cc4
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-21 19:31:11 +0200

    make inputs work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnulaInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+   
+  }
+}
