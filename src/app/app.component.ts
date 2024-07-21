7ef077331c0031864f7139c4ce997996541e5bb7
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-21 20:40:50 +0200

    changes of styling and modify content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './investment-input-model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
     let investmentValue = data.initialInvestment;
@@ -32,6 +42,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  @Input() results?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+}
